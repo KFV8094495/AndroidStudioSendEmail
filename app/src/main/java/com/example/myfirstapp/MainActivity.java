36fd90328d9217fd42a1dff9be62c113dfc2b278
v8094495 <v8094495@live.tees.ac.uk>
2020-01-31 11:21:53 +0000

    Update email

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
      public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText editText = (EditText) findViewById(R.id.editTextMessage);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
